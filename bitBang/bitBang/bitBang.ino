1db82d9b811a647dd42d99d127949327fa195f21
MiataMike <48374237+MiataMike@users.noreply.github.com>
2022-04-29 19:01:18 -0700

    updated firmware to send squarewave, read difference between the two

--- bitBang/bitBang/bitBang.ino
+++ bitBang/bitBang/bitBang.ino
@@ -1,4 +1,4 @@
-#define bignumofreads 1000// This is how many times we'll loop before averaging
+#define bignumofreads 100000// This is how many times we'll loop before averaging
 float integration[4] = {0,0,0,0};
 int step = 0; //counter for output pins' states
 void setup() {
@@ -18,7 +18,7 @@ for (long i = 0;i<bignumofreads;i++)
   switch(step)
   {
     case 0:
-      integration[3] += analogRead(A1);//capture the end of 3
+      integration[1] += analogRead(A1);//capture the end of 3
       digitalWrite(highPin,HIGH);
       digitalWrite(lowPin,HIGH);
       //integration[0] += analogRead(14);
@@ -28,30 +28,16 @@ for (long i = 0;i<bignumofreads;i++)
       digitalWrite(highPin,LOW);
       digitalWrite(lowPin,HIGH);
       break;
-    case 2:
-      integration[1] += analogRead(A1);
-      digitalWrite(highPin,LOW);
-      digitalWrite(lowPin,LOW);
-      break;
-    case 3:
-      integration[2] += analogRead(A1);
-      digitalWrite(highPin,HIGH);
-      digitalWrite(lowPin,LOW);
-      break;
   }
   step++;
-  if(step>=4){step=0;} //reset when it overflows
+  if(step>=2){step=0;} //reset when it overflows
   
   //integration += analogRead(A0);
   //delayMicroseconds(50);
   //delay(10);
 }
-Serial.println("---");
-for(int i = 0; i<4; i++)
-{
-  Serial.println(integration[i]/bignumofreads);
-}
-for(int i = 3; i>=0;i--)
+Serial.println((integration[0]-integration[1])/bignumofreads);
+for(int i = 1; i>=0;i--)
   {
     integration[i]=0;
   }
